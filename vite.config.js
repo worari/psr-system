fb3e55cd5c99a34b49c511b2155e5b0b177780e4
worari <voradorn.aree@gmail.com>
2026-02-25 10:29:43 +0700

    ปรับแก้ไข

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'frontend'),
   plugins: [vue()],
   server: {
     port: 3000,
@@ -10,6 +12,6 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: 'terser',
-    outDir: 'dist'
+    outDir: '../dist'
   }
 })
